38a24c17e1d8630113dbe4412e504b2e5ee88a25
sohnbong <43973824+sohnbong@users.noreply.github.com>
2026-05-11 16:59:01 +0900

    Add files via upload

---
+++ hist.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define NUM_BINS 10
+#define NUM_ELEMENTS (1024*1024*1024)
+
+void genData(float* _data)
+{
+	for (int i = 0; i < NUM_ELEMENTS; i++) 
+		_data[i] = rand()%NUM_BINS + (rand()%100)/100.0f;
+}
+
+int main()
+{
+	double start_time, end_time;
+
+	float* Data=(float*)malloc(sizeof(float)*NUM_ELEMENTS);
+	genData(Data);
+
+	// insert your code for computing histogram.
+
+	free(Data);
+	return 0;
+}
